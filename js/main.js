99f095b10475729ef346a7da8338f4296c7cb0f2
Artiom Mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-01-06 20:35:28 +0300

    add locking body after showing mobile menu

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+const menuSwitcher = document.getElementById('menu-switch');
+const mobileMenu = document.querySelector('.mobile-menu__wrapper');
+
+menuSwitcher.addEventListener('change', (e) => {
+	if (e.target.checked) {
+		bodyLock();
+	} else if (!e.target.checked) {
+		bodyUnlock();
+	}
+});
+mobileMenu.addEventListener('click', (e) => {
+	menuSwitcher.checked = false;
+	bodyUnlock();
+});
+
+function bodyLock() {
+	document.body.classList.add('lock-body');
+}
+function bodyUnlock() {
+	document.body.classList.remove('lock-body');
+
+}
